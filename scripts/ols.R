c93d23db4532ff7ba29bdd8bd2c89747cb62f29c
Youngshin Kim <youngshinysk@gmail.com>
2016-12-01 22:51:46 -0800

    Delete ols.R

--- scripts/ols.R
+++
@@ -1,9 +0,0 @@
-set.seed(159)
-
-edu = read.csv('../data/final.csv', stringsAsFactors = FALSE)
-edu = edu[, -c(1)]
-
-ols = lm(Earning~., data = edu)
-ols_summary = summary(ols)
-
-save(ols_summary, file = '../data/ols.RData')
