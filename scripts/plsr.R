f045555a13f3b98518622c0b9a99b0b2fc2c6b68
acarango <austincarango@gmail.com>
2016-12-04 14:42:55 -0800

    added plotting code to plsr script

--- scripts/plsr.R
+++ scripts/plsr.R
@@ -2,11 +2,18 @@ library(pls)
 
 set.seed(159)
 
+#fit model
 plsr = plsr(Earning~., data = edu, validation ="CV", scale = FALSE,standardize = FALSE)
 
+#find coeffs
 plsr_coef = plsr$coefficients[,1,which.min(plsr$validation$PRESS)]
 plsr_coef
 
+#save data
 save(plsr, file = '../data/plsr.RData')
 
-
+#plot
+pdf("~/stat159/stat159-fall2016-project3/images/plsr.pdf")
+par(mfrow=c(1,1))
+plot(plsr)
+dev.off()
