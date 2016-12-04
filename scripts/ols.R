d194fc4747231dabff3e213979a6963982abe1b2
acarango <austincarango@gmail.com>
2016-12-04 14:40:52 -0800

    added code for pdf plot to ols.R

--- scripts/ols.R
+++ scripts/ols.R
@@ -1,9 +1,18 @@
 set.seed(159)
 
+#read in data
 edu = read.csv('../data/final.csv', stringsAsFactors = FALSE)
 edu = edu[, -c(1)]
 
+#fit model
 ols = lm(Earning~., data = edu)
 ols_summary = summary(ols)
 
+#save data
 save(ols, file = '../data/ols.RData')
+
+#plot
+pdf("../images/ols.pdf")
+par(mfrow=c(2,2))
+plot(ols)
+dev.off()
