a308f977eb9ab1636729dc52934066313423aa24
Youngshin Kim <youngshinysk@gmail.com>
2016-12-04 22:21:50 -0800

    changed index on lasso

--- scripts/lasso.R
+++ scripts/lasso.R
@@ -7,7 +7,9 @@ grid = 10^seq(10, -2, length=100)
 
 set.seed(159)
 
-lasso = cv.glmnet(as.matrix(edu[,c(1:33)]), as.matrix(edu[,34]), intercept = FALSE, 
+lasso = cv.glmnet(as.matrix(edu[,c(1:32)]), as.matrix(edu[,33]), intercept = FALSE, 
                    standardize = FALSE, lambda = grid, alpha = 1)
+
+lasso$glmnet.fit$beta[,which(lasso$lambda == lasso$lambda.min)]
 
 save(lasso, file = '../data/lasso.RData')
