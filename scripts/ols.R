ac3295a75aeb681125a3140066d1ebaab1a95f63
acarango <austincarango@gmail.com>
2016-12-05 11:17:08 -0800

    changed pathing to work with makefile

--- scripts/ols.R
+++ scripts/ols.R
@@ -1,7 +1,7 @@
 set.seed(159)
 
 #read in data
-edu = read.csv('../data/final.csv', stringsAsFactors = FALSE)
+edu = read.csv('data/final.csv', stringsAsFactors = FALSE)
 edu = edu[, -c(1)]
 
 #fit model
@@ -9,10 +9,10 @@ ols = lm(Earning~., data = edu)
 ols_summary = summary(ols)
 
 #save data
-save(ols, file = '../data/ols.RData')
+save(ols, file = 'data/ols.RData')
 
 #plot
-pdf("../images/ols.pdf")
+pdf("images/ols.pdf")
 par(mfrow=c(2,2))
 plot(ols)
 dev.off()
